947516dd6821afa5821cccb2d9a3f9847897d3ba
Adam Mechtley <adam.mechtley@gmail.com>
2013-07-03 10:41:44 -0500

    Changed verbiage

--- __init__.py
+++ __init__.py
@@ -21,5 +21,5 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
-This package contains extension modules for assisting with data mining work.
+This package contains modules for assisting with data mining work.
 """
